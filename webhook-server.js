6c7760219576291bdb552132a8b632b8ef3ca363
danieldjordan <danieldjordan@gmail.com>
2017-06-08 10:38:53 -0500

    removed SLack function

--- webhook-server.js
+++ webhook-server.js
@@ -4,13 +4,11 @@ var restClient = require('node-rest-client').Client;
 const nodemailer = require('nodemailer');
 
 var PORT = (process.env.PORT || 5000);
-var SECURITY_TOKEN = 'OJdqYg87SOcax0baFpf5WInifeErRryPA9qLjiugadBgenwi3UDBj8od21UM5to';
-var HTTP_AUTH_B64_TOKEN = 'dXNlcjEyMzpwYXNzNzg5'; // user123:pass789
-var TARGET_HOOK = 'https://hooks.slack.com/services/<my_target>';
+//var SECURITY_TOKEN = 'OJdqYg87SOcax0baFpf5WInifeErRryPA9qLjiugadBgenwi3UDBj8od21UM5to';
+//var HTTP_AUTH_B64_TOKEN = 'dXNlcjEyMzpwYXNzNzg5'; // user123:pass789
 var te_img = 'https://s3.amazonaws.com/uploads.hipchat.com/6634/194641/uncYbgVEMQ1XNtk/TE-Eye-36x36.jpg';
 var app = express();
 var FROM_ADDRESS = process.env.FROM_NAME  + " <" + process.env.MAIL_RETURN + ">";
-    //Change this to a valid return address
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -25,11 +23,9 @@ let transporter = nodemailer.createTransport({
 });
 
 let mailOptions = {
-  //name: 'ThousandEyes',
-  //address: process.env.MAIL_RETURN, //Sender Address
   from: FROM_ADDRESS,
-  to: process.env.DESTINATION_EMAIL, //Destination Address
-  subject: process.env.SUBJECT,//'ThousandEyes (Sev4)', //Subject Line
+  to: process.env.DESTINATION_EMAIL, 
+  subject: process.env.SUBJECT,
   html: process.env.MAIL_BODY,
 };
 
@@ -44,65 +40,6 @@ function objToStr(obj) {
     }
   }
   return str;
-}
-
-function translateHookContent_slack(req) {
-  var retVal = "";
-  switch (req.body.eventType) {
-    case "ALERT_NOTIFICATION_TRIGGER":
-      retVal = "Alert notification [<" + req.body.alert.permalink + "|" + req.body.alert.alertId + ">]: " + req.body.alert.testName + " (" + req.body.alert.ruleName + ")\n";
-      switch (req.body.alert.type) {
-        case "BGP":
-          break;
-        case "DNS+ Domain":
-          break;
-        case "DNS+ Server":
-          break;
-        default:
-          //this has agents
-          retVal += " " + req.body.alert.agents.length + " agents: ";
-          for (var i = 0; i < req.body.alert.agents.length; i++) {
-            if (i > 0) {
-              retVal += " | ";
-            }
-            retVal += "<" + req.body.alert.agents[i].permalink + "|" + req.body.alert.agents[i].agentName + "> ("
-            if (req.body.alert.agents[i].active == "1") {
-              retVal += "Active";
-            } else {
-              retVal += "Cleared";
-            }
-            retVal += "): " + req.body.alert.agents[i].metricsAtEnd;
-          }
-      }
-      break;
-    case "ALERT_NOTIFICATION_CLEAR":
-      retVal = "Alert cleared [<" + req.body.alert.permalink + "|" + req.body.alert.alertId + "|" + ">]: " + req.body.alert.testName + " (" + req.body.alert.ruleName + ")";
-      break;
-    case "AGENT_ALERT_NOTIFICATION_TRIGGER":
-      retVal = "Agent notification [" + req.body.agentAlert.agentNotificationId + "]: " + req.body.agentAlert.agentName + " (" + req.body.agentAlert.ruleName + ") ";
-      retVal += req.body.agentAlert.hostname + " (" + req.body.agentAlert.metricsAtStart + "): "
-      if (req.body.agentAlert.active == "1") {
-        retVal += "Active";
-      } else {
-        retVal += "Cleared";
-      }
-      break;
-    case "AGENT_ALERT_NOTIFICATION_CLEAR":
-      retVal = "Agent notification cleared [" + req.body.agentAlert.agentNotificationId + "]: " + req.body.agentAlert.agentName + " (" + req.body.agentAlert.ruleName + ") ";
-      retVal += req.body.agentAlert.hostname + " (" + req.body.agentAlert.metricsAtEnd + "): "
-      if (req.body.agentAlert.active == "1") {
-        retVal += "Active";
-      } else {
-        retVal += "Cleared";
-      }
-      break;
-    case "WEBHOOK_TEST":
-      retVal = "Webhook test received. (" + req.body.eventId + ")";
-      break;
-    default:
-      retVal = "Received unregistered event type " + req.body.eventType + " from ThousandEyes webhook.  Body data: \n" + JSON.stringify(req.body);
-  }
-  return ({ username: "ThousandEyes Alerts", icon_url: te_img, text: retVal });
 }
 
 app.get('/', function (request, response) {
@@ -137,9 +74,6 @@ switch (req.body.eventType) {
     return;
 
 }
-
-
-
 });
 
 app.use('/webhook-server', router);
