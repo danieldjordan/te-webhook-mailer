4246af1cc0f796556bf439da5b14e6267c81a7cb
danieldjordan <danieldjordan@gmail.com>
2017-06-23 14:25:08 -0500

    Added comments, and agent notification switch statements

--- webhook-server.js
+++ webhook-server.js
@@ -1,15 +1,23 @@
+
+//Set Variables 
 var express = require('express');
 var bodyParser = require('body-parser');
 var restClient = require('node-rest-client').Client;
 const nodemailer = require('nodemailer');
 
+//Sets port entered as Heroku Variable
 var PORT = (process.env.PORT || 5000);
+//Authentication Settings - Base 64 encoded token 
 var SECURITY_TOKEN = 'OJdqYg87SOcax0baFpf5WInifeErRryPA9qLjiugadBgenwi3UDBj8od21UM5to';
-var HTTP_AUTH_B64_TOKEN = 'dXNlcjEyMzpwYXNzNzg5'; // user123:pass789
+var HTTP_AUTH_B64_TOKEN = 'dXNlcjEyMzpwYXNzNzg5'; // user123:pass789 Linux Command echo -n "username:password" | base64
+//Image for get page
 var te_img = 'https://s3.amazonaws.com/uploads.hipchat.com/6634/194641/uncYbgVEMQ1XNtk/TE-Eye-36x36.jpg';
 var app = express();
+
+//Build the from address from Heroku variable FROM_NAME and MAIL_RETURN
 var FROM_ADDRESS = process.env.FROM_NAME  + " <" + process.env.MAIL_RETURN + ">";
 
+//Set up Nodemailer
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 let transporter = nodemailer.createTransport({
@@ -35,13 +43,17 @@ function objToStr(obj) {
   return str;
 }
 
+//function to process a get request to the base URL
 app.get('/', function (request, response) {
-  response.send('This the ThousandEyes simple Webhook server sample.  Use POST methods instead of GET.')
+  response.send('This the ThousandEyes simple Webhook server sample.  Use POST methods to /sev1.')
   console.log('GET request received');
 })
 
+
+//function to process the te endpoint
 app.post('/te', function (req, res) {
 
+  //build the nodemailer options
   let mailOptions = {
     from: FROM_ADDRESS,
     to: process.env.DESTINATION_EMAIL, 
@@ -49,6 +61,7 @@ app.post('/te', function (req, res) {
     html: process.env.MAIL_BODY,
   };  
 
+  //check for authentication
   var client_ip = req.ip
   if (req.headers['authorization'] !== 'Basic ' + HTTP_AUTH_B64_TOKEN) {
     console.log("Unauthorized Access from " + client_ip)
@@ -56,14 +69,16 @@ app.post('/te', function (req, res) {
     return;
   }
 
+  //switch statement to handle the different TE specific eventTypes
   switch (req.body.eventType) {
 
+    //Webhook test event type
     case "WEBHOOK_TEST":
       console.log(req.body.eventId)
       res.status(200).send(req.body)
       break;
       
-    
+    //Alert Notification event type
     case "ALERT_NOTIFICATION_TRIGGER":
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -77,11 +92,23 @@ app.post('/te', function (req, res) {
       res.status(200).send(req.body)
       break;
 
+    //Alert Notification clear event type
     case "ALERT_NOTIFICATION_CLEAR":
       console.log(req.body.eventId)
       res.status(200).send(req.body)
       break;
-      
+
+    //Agent alert Notification event type
+    case "AGENT_ALERT_NOTIFICATION_TRIGGER":
+      console.log("Agent Alert Recieved")
+      res.status(200).send(req.body)
+      break;
+
+    //Agent alert Notification clear event type
+    case "AGENT_ALERT_NOTIFICATION_CLEAR":
+      console.log("Agent Alert Clear Recieved")
+      res.status(200).send(req.body)
+      break;
   }
 });
 
